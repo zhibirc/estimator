0a175053bec281d27ffcd85d5e0cc5e37ef702e7
Yaroslav Surilov <y.surilov@infomir.com>
2018-09-09 13:20:31 +0300

    add styles for markup, add test templates

---
+++ test/specs.js
@@ -0,0 +1,19 @@
+/**
+ * Mocha tests.
+ *
+ * @license The MIT License (MIT)
+ *
+ * @author Yaroslav Surilov
+ */
+
+'use strict';
+
+/* eslint-env mocha */
+/* eslint-disable max-lines-per-function */
+
+const should = require('should');
+
+describe('main', function () {
+    it('should fail: add task with bad arguments', function () {
+    });
+});
