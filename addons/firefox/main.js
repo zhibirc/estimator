0f5f01a9abea1a4ea5f8bc76458c53aacc5836c7
Yaroslav <y.surilov@infomir.com>
2020-04-11 18:31:41 +0300

    fullfill manifest, add app icon, refactor

--- addons/firefox/main.js
+++ addons/firefox/main.js
@@ -1,12 +1,17 @@
-const doc            = document;
-const fetch          = doc.getElementById.bind(doc);
-const estimateButton = fetch('estimate');
-const result         = fetch('result');
+/**
+ * Main application entry point.
+ *
+ * @module
+ */
+
+const getElement     = document.getElementById.bind(document);
+const estimateButton = getElement('estimate');
+const result         = getElement('result');
 
 estimateButton.addEventListener('click', () => {
-    let optimisticMark  = +fetch('optimistic-mark').value;
-    let normalMark      = +fetch('normal-mark').value;
-    let pessimisticMark = +fetch('pessimistic-mark').value;
+    let optimisticMark  = +getElement('optimistic-mark').value;
+    let normalMark      = +getElement('normal-mark').value;
+    let pessimisticMark = +getElement('pessimistic-mark').value;
 
     result.textContent = ((optimisticMark + 4 * normalMark + pessimisticMark) / 6).toFixed(1) + ' hours';
 });
